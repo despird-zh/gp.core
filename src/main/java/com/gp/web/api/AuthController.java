2eeca7c323f0ee1431450a7c137b58563a170d30
gdiao <gary.diao@yahoo.com>
2017-08-30 08:43:09 +0800

    change the package name

--- src/main/java/com/gp/web/api/AuthController.java
+++ src/main/java/com/gp/web/api/AuthController.java
@@ -1,4 +1,4 @@
-package com.gp.web.service;
+package com.gp.web.api;
 
 import java.util.Date;
 import java.util.Map;
--- src/main/java/com/gp/web/api/TrapAllController.java
+++ src/main/java/com/gp/web/api/TrapAllController.java
@@ -1,4 +1,4 @@
-package com.gp.web.service;
+package com.gp.web.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
